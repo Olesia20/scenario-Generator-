665108aa2bfa9b9bc58f95b939a9e31287fca24c
olesia <infocooll.col@gmail.com>
2026-09-07 19:25:29 +0200

    Added basic JS and typewriter plugin

--- src/script.js
+++ src/script.js
@@ -1,1 +1,13 @@
+function generateScenario(event) {
+  event.preventDefault();
 
+  new Typewriter("#scenario", {
+    strings: "Hello",
+    autoStart: true,
+    delay: 1,
+    cursore: "",
+  });
+}
+
+let generatorForm = document.querySelector("#gnerator-scenario-form");
+generatorForm.addEventListener("submit", generateScenario);
