264021efae1713ab3226d36d7002a00a9b73f568
olesia <infocooll.col@gmail.com>
2026-09-07 23:09:07 +0200

    Update js

--- src/script.js
+++ src/script.js
@@ -13,7 +13,7 @@ function generateScenario(event) {
   let apiKey = "7561c89f6et45b832o8e3c5fcade60b0";
   let prompt = `User instructions: Generate a short movie scenario about ${instractionElement.value}`;
   let context =
-    "You are a creative specialist who writes screenplays for different films. Your mission is to generate a short movie scene in basic HTML . Make sure to follow the user instructions. Write please just scenario and don't write ```html above ";
+    "You are a creative specialist who writes screenplays for different films. Your mission is to generate a short movie scene in basic HTML . Make sure to follow the user instructions. Write please just scenario and don't write ```html above";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   axios.get(apiUrl).then(displayScenario);
 }
